7a468e851840c4fdcd63df180d7d9898aa1dcbdd
ahad <ahadkhalid117@gmail.com>
2024-09-20 00:10:26 +0530

    update in app file

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://143.110.243.191/';
+    public string $baseURL = 'http://143.110.243.191/ci4webapp/';
     // public $baseURL;
 	// public function __construct()
 	// {
