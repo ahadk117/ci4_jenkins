e9bca7d8f3f1aa3debdfce6647d68c9c4613df6f
ahadk117 <ahadkhalid117@gmail.com>
2024-09-24 18:02:10 +0530

    update some files

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -266,7 +266,7 @@
 
         <div class="heroe">
 
-            <h1>Welcome to CodeIgniter new 3<?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+            <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
             <h2>The small framework with powerful features</h2>
 
